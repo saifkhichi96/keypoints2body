a5c198a900c6eaaf5f1f3e42069ecadd0cfd1b32
Saif Khan <saifkhichi96@gmail.com>
2026-02-27 02:21:19 +0100

    feat: add changelog and versioning to keypoints2body package

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+## [0.1.0] - 2026-02-27
+
+### Added
+- First public release of `keypoints2body` as a package.
+- Functional APIs for frame and sequence fitting.
+- CLI commands for frame fitting, sequence fitting, and evaluation.
+- Sphinx documentation and Read the Docs configuration.
+
--- keypoints2body/__init__.py
+++ keypoints2body/__init__.py
@@ -1,3 +1,5 @@
+from importlib.metadata import PackageNotFoundError, version
+
 from .api.frame import optimize_params_frame
 from .api.sequence import optimize_params_sequence, optimize_shape_sequence
 from .models.smpl_data import (
@@ -8,7 +10,13 @@ from .models.smpl_data import (
     SMPLXData,
 )
 
+try:
+    __version__ = version("keypoints2body")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
+
 __all__ = [
+    "__version__",
     "optimize_params_frame",
     "optimize_params_sequence",
     "optimize_shape_sequence",
